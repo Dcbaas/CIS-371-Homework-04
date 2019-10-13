faaa841fab001e97b0d62cabb9d4d50bcadd01e4
David Baas <Dcbaas@users.noreply.github.com>
2019-10-12 21:29:22 -0400

    Added database update definitions. untested

--- src/db-inserter.js
+++ src/db-inserter.js
@@ -1,21 +1,38 @@
 import { AppDB } from "./db-init.js";
 
 const myHandler = function(event) {
-  const descriptionField = document.getElementById('what');
+  const dateField = document.getElementById("doe");
+  const descriptionField = document.getElementById("what");
   const amountField = document.getElementById("amt");
+  const catagoryField = document.getElementById("catagory");
+
+  const date = dateField.value;
   const description = descriptionField.value;
-  const amount = Number(amountField.value); // convert string to number
-  if(description === '' || 
-    description === null || 
-    amount === '' || 
-    amount === null){
-    alert('No values for one of the fields. Nothing commited');
+  const amount = Number(amountField.value);
+  const catagory = catagoryField.value;
+
+  if (
+    date === "" ||
+    date === null ||
+    description === "" ||
+    description === null ||
+    amount === "" ||
+    amount === null ||
+    catagory === "" ||
+    catagory === null
+  ) {
+    alert("No values for one of the fields. Nothing commited");
     return;
   }
-  AppDB.ref('budget')
-  .push()
-  .set({ description: description, amount: amount });
+  AppDB.ref("budget")
+    .push()
+    .set({
+      date: date,
+      description: description,
+      amount: amount,
+      catagory: catagory
+    });
 };
 
-const theButton = document.getElementById('addBtn');
+const theButton = document.getElementById("addBtn");
 theButton.addEventListener("click", myHandler);
